b7101c247be670320826bdc19c306946083f1c86
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2023-10-12 19:03:55 -0600

    Ejercicio4 cuenta regresiva

--- FernandoMagdielChiCanul/js/index_dom.js
+++ FernandoMagdielChiCanul/js/index_dom.js
@@ -1,6 +1,7 @@
 import hamburguerMenu from "./DOM/menu_hamburguesa.js";
 import { digitalClock,alarm } from "./DOM/reloj.js";
 import { moveBall,shortcuts } from "./DOM/teclado.js";
+import countdown from "./DOM/cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,6 +9,8 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     hamburguerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown", "Nov 12, 2023 19:00:40", "Feliz Cumpleaños");
+
 });
 
 d.addEventListener("keydown", (e) =>{
