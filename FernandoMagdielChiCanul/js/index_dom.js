8f9443601b95efe72366813f20ac1e8d211bff47
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2023-10-12 21:06:07 -0600

    Ejercicio5

--- FernandoMagdielChiCanul/js/index_dom.js
+++ FernandoMagdielChiCanul/js/index_dom.js
@@ -2,6 +2,8 @@ import hamburguerMenu from "./DOM/menu_hamburguesa.js";
 import { digitalClock,alarm } from "./DOM/reloj.js";
 import { moveBall,shortcuts } from "./DOM/teclado.js";
 import countdown from "./DOM/cuenta_regresiva.js";
+import scrollTopButton from "./DOM/boton_scroll.js";
+
 
 const d = document;
 
@@ -10,7 +12,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
     countdown("countdown", "Nov 12, 2023 19:00:40", "Feliz Cumpleaños");
-
+    scrollTopButton(".scroll-top-btn")
 });
 
 d.addEventListener("keydown", (e) =>{
