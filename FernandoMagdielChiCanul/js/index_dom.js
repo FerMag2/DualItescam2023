e0f2c896915b5c8d13f25f691dda32445734f0c1
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2023-10-13 08:48:42 -0600

    Ejercicio 9

--- FernandoMagdielChiCanul/js/index_dom.js
+++ FernandoMagdielChiCanul/js/index_dom.js
@@ -6,6 +6,7 @@ import scrollTopButton from "./DOM/boton_scroll.js";
 import darkTheme from "./DOM/tema_oscuro.js";
 import responsiveMedia from "./DOM/objeto_responsive.js";
 import responsiveTester from "./DOM/prueba_responsive.js";
+import userDeviceInfo from "./DOM/deteccion_dispositivos.js";
 
 
 const d = document;
@@ -19,6 +20,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     responsiveMedia("youtube","(min-width: 1024px)",`<a href="https://youtu.be/0kZeoYQd9Dc?si=YzaDcJBYffgiWUEZ" target="_blank" rel="noopener">Ver video</a>`,`<iframe width="560" height="315" src="https://www.youtube.com/embed/0kZeoYQd9Dc?si=8nRY8FgWFk2YThbN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
     responsiveMedia("gmaps","(min-width: 1024px)",`<a href="https://maps.app.goo.gl/rBgd4bnduXzkJT6b7" target="_blank" rel="noopener">Ver mapa</a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238419.68081292012!2d-89.79773368038961!3d20.980306903287538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f56715cab450d17%3A0x5dfc155715abeb09!2zTcOpcmlkYSwgWXVjLg!5e0!3m2!1ses!2smx!4v1697171790799!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
     responsiveTester("responsive-tester");
+    userDeviceInfo("user-device");
 });
 
 d.addEventListener("keydown", (e) =>{
